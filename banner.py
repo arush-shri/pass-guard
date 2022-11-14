57ff8ec9439bd5a211941b3b1fae066254bc0511
Arush Shrivastava <66080016+M4dar4@users.noreply.github.com>
2022-11-14 05:53:50 -0500

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+
+import pyfiglet
+from termcolor import colored
+import random
+import os
+
+#FONT SELECTION
+fonts = ["big","bubble","pagga","script","shadow","slant","small","smblock"]
+
+#COLOR SELECTION
+colors = ["grey","red","green","yellow","blue","magenta","cyan","white"]
+
+ran_font = random.choice(fonts)
+ran_color = random.choice(colors)
+banner = pyfiglet.figlet_format("    PASS GUARD",font = ran_font)
+ascii_art = ('''
+	              _______________
+	              \      _      /         __
+	               \_____()____/         /  )
+	                ===========         /  /
+	               (# O\| |/O #)       /  /
+	                \   (_)   /       /  /
+	                |\ '---' /|      /  /
+	        _______/  \_____/  \____/ o_|
+	       /       \  /     \  /   / o_|
+	      / |           o|        / o_| \\
+	     /  |  _____     |       / /   \ \\
+	    /   |  |===|    o|      / /\    \ \\
+	   |    |   \_/      |     / /  \    \ \\
+	   |    |___________o|__/----)   \    \ \\
+	   |    '              ||  --)    \     |
+	   |___________________||  --)     \    /
+	        |           o|          |   \__/
+	        |            |          |
+''')
+os.system("clear")
+print (colored(ascii_art + "\n\n" + banner , ran_color))
